286ea66865646e0fd5c2e092b3a579918eeffe45
Arnav Kumar <arnavkumarpatna@gmail.com>
2026-03-13 11:58:07 +0530

    UC3: Reverse using loop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -24,4 +24,15 @@ public class PalindromeCheckerApp {
         System.out.println("UC2: Not a Palindrome");
     }
 }
+public static void reverseUsingLoop(String word) {
+    String reversed = "";
+    for (int i = word.length() - 1; i >= 0; i--) {
+        reversed += word.charAt(i);
+    }
+    if (word.equals(reversed)) {
+        System.out.println("UC3: Reverse Loop → Palindrome");
+    } else {
+        System.out.println("UC3: Not Palindrome");
+    }
+}
 }
